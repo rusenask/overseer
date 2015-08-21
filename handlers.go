bae26bd3b3abbdd0748a6921d7d8087082a67ba3
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-21 23:55:35 +0100

    using unrolled/render package for rendering

--- handlers.go
+++ handlers.go
@@ -5,6 +5,7 @@ import (
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/go-zoo/bone"
+	"github.com/unrolled/render"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,15 +15,21 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 		"url_path":  r.URL.Path,
 	}).Info("Getting home view")
 
+	ren := render.New(render.Options{
+		Layout: "layout",
+	})
+
 	w.Header().Set("Content-Type", "application/json")
 	s := &Stubo{&http.Client{}, "localhost", "8001", "http"}
 	s.getScenariosDetail()
-	response := []byte("Hello!")
-	w.Write(response)
+	// response := []byte("Hello!")
+	// w.Write(response)
+	ren.HTML(w, http.StatusOK, "example", nil)
 
 }
 
 func scenarioDetailedHandler(w http.ResponseWriter, r *http.Request) {
+
 	id := bone.GetValue(r, "id")
 	scenario := bone.GetValue(r, "scenario")
 	w.Write([]byte("id:" + string(id) + ", scenario: " + scenario))
