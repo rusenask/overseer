e1e7fd5459011ed4f5cba30cb6c83c3c54b38ac6
Karolis Rusenas <karolis.rusenas@opencredo.com>
2015-08-21 22:00:07 +0100

    initial server commit

---
+++ server.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/negroni"
+	"github.com/go-zoo/bone"
+	"github.com/meatballhat/negroni-logrus"
+)
+
+func main() {
+	// looking for option args when starting App
+	// like ./lgc -port=":3000" would start on port 3000
+	var port = flag.String("port", ":3000", "Server port")
+	flag.Parse() // parse the flag
+
+	log.WithFields(log.Fields{
+		"port": port,
+	}).Info("Overseer is starting")
+
+	mux := bone.New()
+	mux.Get("/", http.HandlerFunc(homeHandler))
+	n := negroni.Classic()
+	n.Use(negronilogrus.NewMiddleware())
+	n.UseHandler(mux)
+	n.Run(*port)
+}
